30b296f1ec12f256dff976a4b31cb7c4221f09a9
Shalitha Lakruwan <69634572+shalithacode@users.noreply.github.com>
2023-10-22 21:58:59 +0530

    Base project modification

--- server.py
+++ server.py
@@ -4,4 +4,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template( "index.html")
+    return render_template("index.html")
+
+
+@app.route("/<string:page_name>")
+def works(page_name):
+    return render_template(page_name)
+
