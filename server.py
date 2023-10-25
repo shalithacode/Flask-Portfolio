4304991166321405f25f2ff93f10e855d987d6b1
Shalitha Lakruwan <69634572+shalithacode@users.noreply.github.com>
2023-10-26 00:54:11 +0530

    adding contact us section

--- server.py
+++ server.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template,request,redirect,url_for
+import csv, os
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'super secret key'
+
+
 
 @app.route("/")
 def index():
@@ -11,3 +15,25 @@ def index():
 def works(page_name):
     return render_template(page_name)
 
+
+@app.route('/submit_contact', methods=['POST'])
+def submit_contact():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        subject = request.form.get('subject')
+        message = request.form.get('message')
+
+        if not email or not subject or not message:
+            return redirect(url_for('contact'))  
+
+        file_exists = os.path.isfile("database.csv")
+
+        with open('database.csv', mode='a',newline='') as db:
+            csv_writer = csv.writer(db, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            if not file_exists:
+                csv_writer.writerow(["email", "subject", "message"])
+            csv_writer.writerow([email, subject, message])
+
+
+
+    return redirect("/thankyou.html")
